b2efffee3d234a3f52e51089e8fbff35046f50b7
Pawan <pawanchourasiya7389@gmail.com>
2026-02-18 12:59:11 +0530

    setting updates

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -28,13 +28,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
-if ALLOWED_HOSTS:
-    ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
-else:
+if not ALLOWED_HOSTS or ALLOWED_HOSTS == [""]:
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
 
 # Application definition
 
